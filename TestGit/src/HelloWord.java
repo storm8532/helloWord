90b9471003ec4bce4fe74aed187af39fbdb2869b
storm8532 <storm8532@sina.com>
2017-02-14 14:05:59 +0800

    test

---
+++ TestGit/src/HelloWord.java
@@ -0,0 +1,12 @@
+
+public class HelloWord {
+
+	public String sayHello() {
+		return "Hello baby";
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println(new HelloWord().sayHello());
+	}
+
+}
